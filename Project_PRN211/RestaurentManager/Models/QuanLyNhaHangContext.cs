51e8e6ff3fdfc3d707382f0da9e3098cd32009f8
toanvk <142169369+toanvk@users.noreply.github.com>
2023-08-21 00:16:41 +0700

    x

---
+++ Project_PRN211/RestaurentManager/Models/QuanLyNhaHangContext.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.Extensions.Configuration;
+
+namespace RestaurentManager.Models
+{
+    public partial class QuanLyNhaHangContext : DbContext
+    {
+        public QuanLyNhaHangContext()
+        {
+        }
+
+        public QuanLyNhaHangContext(DbContextOptions<QuanLyNhaHangContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Account> Accounts { get; set; } = null!;
+        public virtual DbSet<Bill> Bills { get; set; } = null!;
+        public virtual DbSet<BillInfo> BillInfos { get; set; } = null!;
+        public virtual DbSet<Food> Foods { get; set; } = null!;
+        public virtual DbSet<FoodCategory> FoodCategories { get; set; } = null!;
+        public virtual DbSet<TableFood> TableFoods { get; set; } = null!;
+
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			Console.WriteLine(Directory.GetCurrentDirectory());
+			IConfiguration config = new ConfigurationBuilder()
+			.SetBasePath(Directory.GetCurrentDirectory())
+			.AddJsonFile("appsettings.json", true, true)
+			.Build();
+			var strConn = config["ConnectionStrings:MyDatabase"];
+			optionsBuilder.UseSqlServer(strConn);
+		}
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Account>(entity =>
+            {
+                entity.HasKey(e => e.UserName)
+                    .HasName("PK__Accounts__C9F28457A2E6345E");
+
+                entity.Property(e => e.UserName).HasMaxLength(100);
+
+                entity.Property(e => e.DisplayName)
+                    .HasMaxLength(100)
+                    .HasDefaultValueSql("(N'Group 6')");
+
+                entity.Property(e => e.PassWord)
+                    .HasMaxLength(1000)
+                    .HasDefaultValueSql("((0))");
+            });
+
+            modelBuilder.Entity<Bill>(entity =>
+            {
+                entity.ToTable("Bill");
+
+                entity.Property(e => e.Id).HasColumnName("id");
+
+                entity.Property(e => e.DateCheckIn)
+                    .HasColumnType("date")
+                    .HasDefaultValueSql("(getdate())");
+
+                entity.Property(e => e.DateCheckOut).HasColumnType("date");
+
+                entity.Property(e => e.Discount)
+                    .HasColumnName("discount")
+                    .HasDefaultValueSql("((0))");
+
+                entity.Property(e => e.IdTable).HasColumnName("idTable");
+
+                entity.Property(e => e.Status).HasColumnName("status");
+
+                entity.Property(e => e.TotalBill)
+                    .HasColumnType("money")
+                    .HasColumnName("totalBill")
+                    .HasDefaultValueSql("((0))");
+
+                entity.HasOne(d => d.IdTableNavigation)
+                    .WithMany(p => p.Bills)
+                    .HasForeignKey(d => d.IdTable)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK__Bill__idTable__4CA06362");
+            });
+
+            modelBuilder.Entity<BillInfo>(entity =>
+            {
+                entity.ToTable("BillInfo");
+
+                entity.Property(e => e.Id).HasColumnName("id");
+
+                entity.Property(e => e.Count).HasColumnName("count");
+
+                entity.Property(e => e.IdBill).HasColumnName("idBill");
+
+                entity.Property(e => e.IdFood).HasColumnName("idFood");
+
+                entity.HasOne(d => d.IdBillNavigation)
+                    .WithMany(p => p.BillInfos)
+                    .HasForeignKey(d => d.IdBill)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK__BillInfo__idBill__4D94879B");
+
+                entity.HasOne(d => d.IdFoodNavigation)
+                    .WithMany(p => p.BillInfos)
+                    .HasForeignKey(d => d.IdFood)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK__BillInfo__idFood__4E88ABD4");
+            });
+
+            modelBuilder.Entity<Food>(entity =>
+            {
+                entity.ToTable("Food");
+
+                entity.Property(e => e.Id).HasColumnName("id");
+
+                entity.Property(e => e.IdCategory).HasColumnName("idCategory");
+
+                entity.Property(e => e.Name)
+                    .HasMaxLength(100)
+                    .HasColumnName("name")
+                    .HasDefaultValueSql("(N'Chưa đặt tên')");
+
+                entity.HasOne(d => d.IdCategoryNavigation)
+                    .WithMany(p => p.Foods)
+                    .HasForeignKey(d => d.IdCategory)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK__Food__idCategory__4F7CD00D");
+            });
+
+            modelBuilder.Entity<FoodCategory>(entity =>
+            {
+                entity.ToTable("FoodCategory");
+
+                entity.Property(e => e.Id).HasColumnName("id");
+
+                entity.Property(e => e.Name)
+                    .HasMaxLength(100)
+                    .HasColumnName("name")
+                    .HasDefaultValueSql("(N'Chưa đặt tên')");
+            });
+
+            modelBuilder.Entity<TableFood>(entity =>
+            {
+                entity.ToTable("TableFood");
+
+                entity.Property(e => e.Id).HasColumnName("id");
+
+                entity.Property(e => e.Name)
+                    .HasMaxLength(100)
+                    .HasColumnName("name")
+                    .HasDefaultValueSql("(N'Chưa đặt tên')");
+
+                entity.Property(e => e.Status)
+                    .HasMaxLength(100)
+                    .HasColumnName("status")
+                    .HasDefaultValueSql("(N'Trống')");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
